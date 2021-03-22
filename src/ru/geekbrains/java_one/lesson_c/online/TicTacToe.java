7b2c218d9bf99356692ccba7818f0a98cb03dbb6
Natalia Brik <natalia.brik@yahoo.com>
2021-03-22 09:51:05 +0100

    Lesson 3: online TickTackToe the beginning

---
+++ src/ru/geekbrains/java_one/lesson_c/online/TicTacToe.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.java_one.lesson_c.online;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '.';
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+    private static int fieldSizeY;
+    private static int fieldSizeX;
+    private static char[][] field;
+
+    public static void main(String[] args) {
+
+    }
+}
