182a8218f0ff061bc1d381ba3db2326164a04abf
Natalia Brik <natalia.brik@yahoo.com>
2021-03-23 13:42:34 +0100

    Lesson 3 recording TicTacToe continueing

--- src/ru/geekbrains/java_one/lesson_c/online/TicTacToe.java
+++ src/ru/geekbrains/java_one/lesson_c/online/TicTacToe.java
@@ -14,6 +14,35 @@ public class TicTacToe {
     private static int fieldSizeX;
     private static char[][] field;
 
+    private static void initField() {
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printField() {
+        System.out.print("+");
+        for (int x = 0; x < fieldSizeX * 2 + 1; x++)
+            System.out.print((x % 2 == 0) ? "-" : x / 2 + 1);
+        System.out.println();
+
+        for (int y = 0; y < fieldSizeY; y++) {
+            System.out.print(y + 1 + "|");
+            for (int x = 0; x < fieldSizeX; x++)
+                System.out.print(field[y][x] + "|");
+            System.out.println();
+        }
+
+        for (int x = 0; x <= fieldSizeX * 2 + 1; x++)
+            System.out.print("-");
+        System.out.println();
+    }
+    
     public static void main(String[] args) {
 
     }
