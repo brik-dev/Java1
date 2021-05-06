625926f1b5b4beb6d59bf342f463ba1f501d5fec
Natalia Brik <natalia.brik@yahoo.com>
2021-05-06 11:21:28 +0200

    Lesson 4: OOP

---
+++ src/ru/geekbrains/java_one/lesson_d/homework/Cat.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_one.lesson_d.homework;
+
+public class Cat {
+    String name;
+    int age;
+    String color;
+}
---
+++ src/ru/geekbrains/java_one/lesson_d/homework/Main.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.java_one.lesson_d.homework;
+
+public class Main {
+}
