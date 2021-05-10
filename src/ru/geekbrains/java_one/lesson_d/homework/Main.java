fc6673fb9e399ded721321d38ec237cf08e476c1
Natalia Brik <natalia.brik@yahoo.com>
2021-05-10 10:38:00 +0200

    Lesson 4: OOP continue

--- src/ru/geekbrains/java_one/lesson_d/homework/Main.java
+++ src/ru/geekbrains/java_one/lesson_d/homework/Main.java
@@ -8,7 +8,16 @@ public class Main {
         c0.color = "White";
         c0.age = 5;
 
-        System.out.printf("Cat: %s is %s of age %d", c0.name, c0.color, c0.age);
+        Cat c1 = new Cat();
+        c1.name = "Murzik";
+        c1.color = "Black";
+        c1.age = 2;
+
+        Cat c2 = c0;
+        c2.age = 2;
+
+        System.out.printf("Cat: %s is %s of age %d\n", c0.name, c0.color, c0.age);
+        System.out.printf("Cat: %s is %s of age %d\n", c1.name, c1.color, c1.age);
 
     }
 }
